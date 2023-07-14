4e4c229c317adf336ecf2c1635ae877c545c2cd0
Hayden James Cooper <haydencpr95@gmail.com>
2023-07-14 16:54:58 -0400

    Inline styling

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
-    <footer>
-      <p>Contact Information</p>
-      <p>Phone Number: 675-454-9789</p>
-      <p>Email: salmonbamcookies@gmail.com</p>
-      <p>10321 W McDowell Rd Seattle, WA 98451</p>
+    <footer style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start' }}>
+      <p style={{ margin: 0, paddingRight: '10px' }}>Contact Information</p>
+      <p style={{ margin: 0, paddingRight: '10px' }}>Phone Number: 675-454-9789</p>
+      <p style={{ margin: 0, paddingRight: '10px' }}>Email: salmonbamcookies@gmail.com</p>
+      <p style={{ margin: 0, paddingRight: '10px' }}>10321 W McDowell Rd Seattle, WA 98451</p>
     </footer>
   );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -2,18 +2,18 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav style={{ display: 'flex', justifyContent: 'center' }}>
+      <ul style={{ display: 'flex', listStyle: 'none', padding: 0 }}>
+        <li style={{ marginRight: '10px' }}>
           <Link href="/">Home</Link>
         </li>
-        <li>
+        <li style={{ marginRight: '10px' }}>
           <Link href="/sales">Sales Data</Link>
         </li>
-        <li>
+        <li style={{ marginRight: '10px' }}>
           <Link href="/stores">Locations</Link>
         </li>
-        <li>
+        <li style={{ marginRight: '10px' }}>
           <Link href="/contact">Contact Us</Link>
         </li>
       </ul>
